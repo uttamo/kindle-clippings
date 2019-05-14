9e70622aa2593c7881db276d4d4ff4c915104b7c
Uttam <uttam@fundment.com>
2019-05-14 08:58:41 +0100

    Cleaned up tests, added gitignore

--- tests/test_notes.py
+++ tests/test_notes.py
@@ -1,16 +1,10 @@
 from unittest import TestCase
 import datetime as dt
-import os, os.path
-from pdb import set_trace as bp
-from pprint import pprint
 
 from notes import _parse_title_and_author_name, _parse_highlight_info, _parse_highlighted_text
 
 
 class TestNotes(TestCase):
-    def setUp(self) -> None:
-        pass
-
     def test_parse_title_and_author_name(self):
         s1 = 'The Billion Dollar Spy: A True Story of Cold War Espionage and Betrayal (David E. Hoffman)'
         assert _parse_title_and_author_name(s1) == \
@@ -29,4 +23,3 @@ class TestNotes(TestCase):
     def test_parse_highlighted_text(self):
         s1 = "Cohen was a little different. He wasn’t particularly fantastic at math, he didn’t study global economies, he had no unique investing philosophy. He was just a great trader,"
         assert _parse_highlighted_text(s1) == "Cohen was a little different. He wasn’t particularly fantastic at math, he didn’t study global economies, he had no unique investing philosophy. He was just a great trader"
-
